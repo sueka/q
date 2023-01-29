e7e4a684e110795b191b08ad43271561b1bbb99d
sueka <mail@sueka.me>
2023-01-30 05:42:28 +0900

    :sparkles: Add arithmetic operators

--- src/lib/Q.spec.ts
+++ src/lib/Q.spec.ts
@@ -14,4 +14,16 @@ describe('Q', () => {
       expect(Q.of(-6351638n, -123n)).toMatchObject({ nu:  154918n, de: 3n })
     })
   })
+
+  describe('plus(), minus(), times() and dividedBy()', () => {
+    it('works', () => {
+      const oneHalf = Q.of(1n, 2n)
+      const twoFifths = Q.of(2n, 5n)
+
+      expect(oneHalf.plus(twoFifths)).toMatchObject({ nu: 9n, de: 10n })
+      expect(oneHalf.minus(twoFifths)).toMatchObject({ nu: 1n, de: 10n })
+      expect(oneHalf.times(twoFifths)).toMatchObject({ nu: 1n, de: 5n })
+      expect(oneHalf.dividedBy(twoFifths)).toMatchObject({ nu: 5n, de: 4n })
+    })
+  })
 })
--- src/lib/Q.ts
+++ src/lib/Q.ts
@@ -10,6 +10,58 @@ export default class Q {
     assert(de !== 0n)
   }
 
+  /**
+   * Calculates a total of `this` and `that`.
+   *
+   * @param that
+   * @returns the reduced total
+   */
+  plus(that: Q): Q {
+    const nu = that.de * this.nu + this.de * that.nu
+    const de = this.de * that.de
+
+    return new Q(nu, de).reduced
+  }
+
+  /**
+   * Calculates a difference between `this` and `that`.
+   *
+   * @param that the subtrahend
+   * @returns the reduced difference
+   */
+  minus(that: Q): Q {
+    const nu = that.de * this.nu - this.de * that.nu
+    const de = this.de * that.de
+
+    return new Q(nu, de).reduced
+  }
+
+  /**
+   * Calculates a product of `this` and `that`.
+   *
+   * @param that
+   * @returns the reduced product
+   */
+  times(that: Q): Q {
+    const nu = this.nu * that.nu
+    const de = this.de * that.de
+
+    return new Q(nu, de).reduced
+  }
+
+  /**
+   * Calculates a quatient of `this` divided by `that`.
+   *
+   * @param that the divisor
+   * @returns the reduced quatient
+   */
+  dividedBy(that: Q) {
+    const nu = this.nu * that.de
+    const de = this.de * that.nu
+
+    return new Q(nu, de).reduced
+  }
+
   get reduced() {
     const deSign = (this.de < 0n) ? -1n : 1n
     const d = deSign * abs(gcd(this.nu, this.de))
