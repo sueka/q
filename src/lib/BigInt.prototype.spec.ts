a232e6edb07e6852be5ef0e6b7641fc981226372
sueka <mail@sueka.me>
2023-02-01 10:03:07 +0900

    :sparkles: Extend bigint for subtraction, multiplication and division

--- src/lib/BigInt.prototype.spec.ts
+++ src/lib/BigInt.prototype.spec.ts
@@ -2,9 +2,12 @@ import Q from './Q'
 import './BigInt.prototype'
 
 describe('BigInt.prototype', () => {
-  describe('plus()', () => {
+  describe('plus(), minus(), times() and dividedBy()', () => {
     it('works', () => {
       expect(4n.plus(Q.of(1n, 2n))).toEqual({ nu: 9n, de: 2n })
+      expect(4n.minus(Q.of(1n, 2n))).toEqual({ nu: 7n, de: 2n })
+      expect(4n.times(Q.of(1n, 2n))).toEqual({ nu: 2n, de: 1n })
+      expect(4n.dividedBy(Q.of(1n, 2n))).toEqual({ nu: 8n, de: 1n })
     })
   })
 })
--- src/lib/BigInt.prototype.ts
+++ src/lib/BigInt.prototype.ts
@@ -3,6 +3,9 @@ import Q from './Q'
 declare global {
   interface BigInt {
     plus(that: Q): Q
+    minus(that: Q): Q
+    times(that: Q): Q
+    dividedBy(that: Q): Q
   }
 }
 
@@ -10,6 +13,21 @@ Object.defineProperties(BigInt.prototype, {
   plus: {
     value(this: bigint, that: Q) {
       return that.plus(this)
+    },
+  },
+  minus: {
+    value(this: bigint, that: Q) {
+      return that.minus(this).opposite
+    },
+  },
+  times: {
+    value(this: bigint, that: Q) {
+      return that.times(this)
+    },
+  },
+  dividedBy: {
+    value(this: bigint, that: Q) {
+      return that.dividedBy(this).reciprocal
     },
   },
 })
